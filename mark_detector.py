9a9f1b1e01cbc7de4ace6a3d45ecf0288b290e8f
yinguobing <yinguobing@gmail.com>
2017-12-27 15:53:22 +0800

    add function to draw all face detection result

--- mark_detector.py
+++ mark_detector.py
@@ -10,6 +10,7 @@ DNN_MODEL = 'assets/res10_300x300_ssd_iter_140000.caffemodel'
 
 FACE_NET = cv2.dnn.readNetFromCaffe(DNN_PROTOTXT, DNN_MODEL)
 
+
 def get_faceboxes(image=None, threshold=0.5):
     """
     Get the bounding box of faces in image using dnn.
@@ -36,3 +37,23 @@ def get_faceboxes(image=None, threshold=0.5):
                 [x_left_bottom, y_left_bottom, x_right_top, y_right_top])
 
     return confidences, faceboxes
+
+
+def draw_all_result(image, confidences, faceboxes):
+    """Draw the detection result on image"""
+    for result in zip(confidences, faceboxes):
+        conf = result[0]
+        facebox = result[1]
+
+        cv2.rectangle(image, (facebox[0], facebox[1]),
+                      (facebox[2], facebox[3]), (0, 255, 0))
+        label = "face: %.4f" % conf
+        label_size, base_line = cv.getTextSize(
+            label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+
+        cv2.rectangle(image, (facebox[0], facebox[1] - label_size[1]),
+                      (facebox[0] + label_size[0],
+                       facebox[1] + base_line),
+                      (0, 255, 0), cv2.FILLED)
+        cv2.putText(image, label, (facebox[0], facebox[1]),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
