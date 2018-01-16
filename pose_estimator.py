91399fb5e21d78f2fe9c19491cd70ad076805d6b
yinguobing <yinguobing@gmail.com>
2018-01-16 11:33:12 +0800

    add rest function to class

--- pose_estimator.py
+++ pose_estimator.py
@@ -1,6 +1,7 @@
 """Estimate head pose according to the facial landmarks"""
-import cv2
 import numpy as np
+
+import cv2
 
 
 class PoseEstimator:
@@ -30,87 +31,82 @@ class PoseEstimator:
         # Assuming no lens distortion
         self.dist_coeefs = np.zeros((4, 1))
 
+    def _get_full_model_points(self, filename='assets/model.txt'):
+        """Get all 68 3D model points from file"""
+        raw_value = []
+        with open(filename) as file:
+            for line in file:
+                raw_value.append(line)
+        model_points = np.array(raw_value, dtype=np.float32)
+        model_points = np.reshape(model_points, (3, -1)).T
+        return model_points
 
-def get_full_model_points(filename='assets/model.txt'):
-    """Get all 68 3D model points from file"""
-    raw_value = []
-    with open(filename) as file:
-        for line in file:
-            raw_value.append(line)
-    model_points = np.array(raw_value, dtype=np.float32)
-    model_points = np.reshape(model_points, (3, -1)).T
-    return model_points
+    def solve_pose(self, image_points):
+        """
+        Solve pose from image points
+        Return (rotation_vector, translation_vector) as pose.
+        """
+        (success, rotation_vector, translation_vector) = cv2.solvePnP(
+            self.model_points, image_points, self.camera_matrix, self.dist_coeefs)
 
+        return (rotation_vector, translation_vector)
 
-def solve_pose(image_points):
-    """
-    Solve pose from image points
-    Return (rotation_vector, translation_vector) as pose.
-    """
-    (success, rotation_vector, translation_vector) = cv2.solvePnP(
-        MODEL_POINTS, image_points, CAMERA_MATRIX, DIST_COEFFS)
+    def solve_pose_by_68_points(self, image_points):
+        """
+        Solve pose from all the 68 image points
+        Return (rotation_vector, translation_vector) as pose.
+        """
+        model_points_68 = self._get_full_model_points()
+        (success, rotation_vector, translation_vector) = cv2.solvePnP(
+            model_points_68, image_points, self.camera_matrix, self.dist_coeefs)
 
-    return (rotation_vector, translation_vector)
+        return (rotation_vector, translation_vector)
 
+    def draw_annotation_box(self, image, rotation_vector, translation_vector, color=(255, 255, 255)):
+        """Draw a 3D box as annotation of pose"""
+        point_3d = []
+        rear_size = 300
+        rear_depth = 0
+        point_3d.append((-rear_size, -rear_size, rear_depth))
+        point_3d.append((-rear_size, rear_size, rear_depth))
+        point_3d.append((rear_size, rear_size, rear_depth))
+        point_3d.append((rear_size, -rear_size, rear_depth))
+        point_3d.append((-rear_size, -rear_size, rear_depth))
 
-def solve_pose_by_68_points(image_points):
-    """
-    Solve pose from all the 68 image points
-    Return (rotation_vector, translation_vector) as pose.
-    """
-    model_points_68 = get_full_model_points()
-    (success, rotation_vector, translation_vector) = cv2.solvePnP(
-        model_points_68, image_points, CAMERA_MATRIX, DIST_COEFFS)
+        front_size = 400
+        front_depth = 400
+        point_3d.append((-front_size, -front_size, front_depth))
+        point_3d.append((-front_size, front_size, front_depth))
+        point_3d.append((front_size, front_size, front_depth))
+        point_3d.append((front_size, -front_size, front_depth))
+        point_3d.append((-front_size, -front_size, front_depth))
+        point_3d = np.array(point_3d, dtype=np.float).reshape(-1, 3)
 
-    return (rotation_vector, translation_vector)
+        # Map to 2d image points
+        (point_2d, _) = cv2.projectPoints(point_3d,
+                                          rotation_vector,
+                                          translation_vector,
+                                          self.camera_matrix,
+                                          self.dist_coeefs)
+        point_2d = np.int32(point_2d.reshape(-1, 2))
 
+        # Draw all the lines
+        cv2.polylines(image, [point_2d], True, color, 1, cv2.LINE_AA)
+        cv2.line(image, tuple(point_2d[1]), tuple(
+            point_2d[6]), color, 1, cv2.LINE_AA)
+        cv2.line(image, tuple(point_2d[2]), tuple(
+            point_2d[7]), color, 1, cv2.LINE_AA)
+        cv2.line(image, tuple(point_2d[3]), tuple(
+            point_2d[8]), color, 1, cv2.LINE_AA)
+        return image
 
-def draw_annotation_box(image, rotation_vector, translation_vector, color=(255, 255, 255)):
-    """Draw a 3D box as annotation of pose"""
-    point_3d = []
-    rear_size = 300
-    rear_depth = 0
-    point_3d.append((-rear_size, -rear_size, rear_depth))
-    point_3d.append((-rear_size, rear_size, rear_depth))
-    point_3d.append((rear_size, rear_size, rear_depth))
-    point_3d.append((rear_size, -rear_size, rear_depth))
-    point_3d.append((-rear_size, -rear_size, rear_depth))
-
-    front_size = 400
-    front_depth = 400
-    point_3d.append((-front_size, -front_size, front_depth))
-    point_3d.append((-front_size, front_size, front_depth))
-    point_3d.append((front_size, front_size, front_depth))
-    point_3d.append((front_size, -front_size, front_depth))
-    point_3d.append((-front_size, -front_size, front_depth))
-    point_3d = np.array(point_3d, dtype=np.float).reshape(-1, 3)
-
-    # Map to 2d image points
-    (point_2d, _) = cv2.projectPoints(point_3d,
-                                      rotation_vector,
-                                      translation_vector,
-                                      CAMERA_MATRIX,
-                                      DIST_COEFFS)
-    point_2d = np.int32(point_2d.reshape(-1, 2))
-
-    # Draw all the lines
-    cv2.polylines(image, [point_2d], True, color, 1, cv2.LINE_AA)
-    cv2.line(image, tuple(point_2d[1]), tuple(
-        point_2d[6]), color, 1, cv2.LINE_AA)
-    cv2.line(image, tuple(point_2d[2]), tuple(
-        point_2d[7]), color, 1, cv2.LINE_AA)
-    cv2.line(image, tuple(point_2d[3]), tuple(
-        point_2d[8]), color, 1, cv2.LINE_AA)
-    return image
-
-
-def get_pose_marks(marks):
-    """Get marks ready for pose estimation from 68 marks"""
-    pose_marks = []
-    pose_marks.append(marks[30])    # Nose tip
-    pose_marks.append(marks[8])     # Chin
-    pose_marks.append(marks[36])    # Left eye left corner
-    pose_marks.append(marks[45])    # Right eye right corner
-    pose_marks.append(marks[48])    # Left Mouth corner
-    pose_marks.append(marks[54])    # Right mouth corner
-    return pose_marks
+    def get_pose_marks(self, marks):
+        """Get marks ready for pose estimation from 68 marks"""
+        pose_marks = []
+        pose_marks.append(marks[30])    # Nose tip
+        pose_marks.append(marks[8])     # Chin
+        pose_marks.append(marks[36])    # Left eye left corner
+        pose_marks.append(marks[45])    # Right eye right corner
+        pose_marks.append(marks[48])    # Left Mouth corner
+        pose_marks.append(marks[54])    # Right mouth corner
+        return pose_marks
