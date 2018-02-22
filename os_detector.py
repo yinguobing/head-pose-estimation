b472aabd4a92b75d2228795c55492c7e7a501af3
yinguobing <yinguobing@gmail.com>
2018-02-22 15:54:45 +0800

    add output for linux os

--- os_detector.py
+++ os_detector.py
@@ -13,3 +13,5 @@ def detect_os(bypass=False):
         print("It seems that you are running this code from {}, on which the Python multithreading may not work properly. Consider running this code on Linux.".format(os_name))
         print("Exiting..")
         exit()
+    else:
+        print("Linux is fine! Python multithreading works.")
