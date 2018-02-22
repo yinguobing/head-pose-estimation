31c40d3363e6dbd7976e63ba72c27238762e5259
yinguobing <yinguobing@gmail.com>
2018-02-22 16:04:01 +0800

    typo fix: -> multiprocessing

--- os_detector.py
+++ os_detector.py
@@ -3,15 +3,15 @@ from platform import system
 
 
 def detect_os(bypass=False):
-    """Check OS, as multithreading may not work properly on Windows and macOS"""
+    """Check OS, as multiprocessing may not work properly on Windows and macOS"""
     if bypass is True:
         return
 
     os_name = system()
 
     if os_name in ['Windows']:
-        print("It seems that you are running this code from {}, on which the Python multithreading may not work properly. Consider running this code on Linux.".format(os_name))
+        print("It seems that you are running this code from {}, on which the Python multiprocessing may not work properly. Consider running this code on Linux.".format(os_name))
         print("Exiting..")
         exit()
     else:
-        print("Linux is fine! Python multithreading works.")
+        print("Linux is fine! Python multiprocessing works.")
