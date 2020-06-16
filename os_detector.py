9b021d0e7b5031446f0c605576f7cb96bf645000
yinguobing <yinguobing@gmail.com>
2020-06-16 09:49:13 +0800

    fixed typo

--- os_detector.py
+++ os_detector.py
@@ -3,14 +3,14 @@ from platform import system
 
 
 def detect_os(bypass=False):
-    """Check OS, as multiprocessing may not work properly on Windows and macOS"""
+    """Check OS, as multiprocessing may not work properly in Windows and macOS"""
     if bypass is True:
         return
 
     os_name = system()
 
     if os_name in ['Windows']:
-        print("It seems that you are running this code from {}, on which the Python multiprocessing may not work properly. Consider running this code on Linux.".format(os_name))
+        print("It seems that you are running this code from {}, in which the Python multiprocessing may not work properly. Consider running this code in Linux.".format(os_name))
         print("Exiting..")
         exit()
     else:
