6691e0969173e5242c59e9f891e42ed0a316cf3e
yinguobing <yinguobing@gmail.com>
2021-09-04 17:01:27 +0800

    no more stabilizers

--- estimate_head_pose.py
+++ estimate_head_pose.py
@@ -11,11 +11,9 @@ https://github.com/yinguobing/head-pose-estimation
 from argparse import ArgumentParser
 
 import cv2
-import numpy as np
 
 from mark_detector import MarkDetector
 from pose_estimator import PoseEstimator
-from stabilizer import Stabilizer
 
 print(__doc__)
 print("OpenCV version: {}".format(cv2.__version__))
@@ -35,14 +33,14 @@ if __name__ == '__main__':
     # 1. Setup the video source from webcam or video file.
     video_src = args.cam if args.cam is not None else args.video
     if video_src is None:
-        print("Warning: video source not assigned, default webcam will be used.")
+        print("Video source not assigned, default webcam will be used.")
         video_src = 0
 
     cap = cv2.VideoCapture(video_src)
 
     # Get the frame size. This will be used by the pose estimator.
-    _, sample_frame = cap.read()
-    height, width = sample_frame.shape[:2]
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
     # 2. Introduce a pose estimator to solve pose.
     pose_estimator = PoseEstimator(img_size=(height, width))
@@ -50,14 +48,7 @@ if __name__ == '__main__':
     # 3. Introduce a mark detector to detect landmarks.
     mark_detector = MarkDetector()
 
-    # 4. Introduce scalar stabilizers for stable pose.
-    pose_stabilizers = [Stabilizer(
-        state_num=2,
-        measure_num=1,
-        cov_process=0.1,
-        cov_measure=0.1) for _ in range(6)]
-
-    # 5. Measure the performance with a tick meter.
+    # 4. Measure the performance with a tick meter.
     tm = cv2.TickMeter()
 
     # Now, let the frames flow.
@@ -83,45 +74,36 @@ if __name__ == '__main__':
             x1, y1, x2, y2 = facebox
             face_img = frame[y1: y2, x1: x2]
 
-            # Now, run the detection.
+            # Run the detection.
             tm.start()
             marks = mark_detector.detect_marks(face_img)
             tm.stop()
 
-            # Convert the marks locations from local CNN to global image.
+            # Convert the locations from local face area to the global image.
             marks *= (x2 - x1)
             marks[:, 0] += x1
             marks[:, 1] += y1
 
-            # Uncomment following line to show raw marks.
-            mark_detector.draw_marks(frame, marks, color=(0, 255, 0))
-
-            # Uncomment following line to show facebox.
-            mark_detector.draw_box(frame, [facebox])
-
             # Try pose estimation with 68 points.
             pose = pose_estimator.solve_pose_by_68_points(marks)
 
-            # Stabilize the pose.
-            steady_pose = []
-            pose_np = np.array(pose).flatten()
-            for value, ps_stb in zip(pose_np, pose_stabilizers):
-                ps_stb.update([value])
-                steady_pose.append(ps_stb.state[0])
-            steady_pose = np.reshape(steady_pose, (-1, 3))
+            # All done. The best way to show the result would be drawing the
+            # pose on the frame in realtime.
 
-            # Uncomment following line to draw pose annotation on frame.
+            # Do you want to see the pose annotation?
             pose_estimator.draw_annotation_box(
-                frame, pose[0], pose[1], color=(255, 128, 128))
+                frame, pose[0], pose[1], color=(0, 255, 0))
 
-            # Uncomment following line to draw stabile pose annotation on frame.
-            pose_estimator.draw_annotation_box(
-                frame, steady_pose[0], steady_pose[1], color=(128, 255, 128))
+            # Do you want to see the head axes?
+            # pose_estimator.draw_axes(frame, pose[0], pose[1])
 
-            # Uncomment following line to draw head axes on frame.
-            pose_estimator.draw_axes(frame, steady_pose[0], steady_pose[1])
+            # Do you want to see the marks?
+            # mark_detector.draw_marks(frame, marks, color=(0, 255, 0))
+
+            # Do you want to see the facebox?
+            # mark_detector.draw_box(frame, [facebox])
 
         # Show preview.
         cv2.imshow("Preview", frame)
-        if cv2.waitKey(10) == 27:
+        if cv2.waitKey(1) == 27:
             break
