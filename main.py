e8512b48ace7196176fbd6b102bee92c93a4b10c
yinguobing <yinguobing@gmail.com>
2023-06-06 15:27:52 +0800

    switch to face detector SCRFD

--- main.py
+++ main.py
@@ -37,20 +37,20 @@ def run():
     # Initialize the video source from webcam or video file.
     video_src = args.cam if args.video is None else args.video
     cap = cv2.VideoCapture(video_src)
+    print(f"Video source: {video_src}")
 
     # Get the frame size. This will be used by the following detectors.
     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
     # Setup a face detector to detect human faces.
-    face_detector = FaceDetector(
-        "assets/face_detection_yunet_2022mar.onnx", [frame_width, frame_height])
+    face_detector = FaceDetector("assets/face_detector.onnx")
 
     # Setup a mark detector to detect landmarks.
-    mark_detector = MarkDetector("assets/facemarks.onnx")
+    mark_detector = MarkDetector("assets/face_landmarks.onnx")
 
     # Setup a pose estimator to solve pose.
-    pose_estimator = PoseEstimator(img_size=(frame_height, frame_width))
+    pose_estimator = PoseEstimator(frame_width, frame_height)
 
     # Measure the performance with a tick meter.
     tm = cv2.TickMeter()
@@ -68,33 +68,29 @@ def run():
             frame = cv2.flip(frame, 2)
 
         # Step 1: Get faces from current frame.
-        faces = face_detector.detect(frame)
+        faces, _ = face_detector.detect(frame, 0.7)
 
-        # Any face found?
-        if faces is not None:
+        # Any valid face found?
+        if len(faces) > 0:
             tm.start()
 
             # Step 2: Detect landmarks. Crop and feed the face area into the
-            # mark detector.
-            refined = refine(faces, frame_width, frame_height)
-            num_faces = refined.shape[0]
-            patches = []
-            for face in refined:
-                x, y, w, h = face[:4].astype(int)
-                patch = frame[y:y+h, x:x+w]
-                patches.append(patch)
+            # mark detector. Note only the first face will be used for
+            # demonstration.
+            face = refine(faces, frame_width, frame_height, 0.15)[0]
+            x1, y1, x2, y2 = face[:4].astype(int)
+            patch = frame[y1:y2, x1:x2]
 
-            # Run the detection.
-            marks = mark_detector.detect(patches)
+            # Run the mark detection.
+            marks = mark_detector.detect([patch])[0].reshape([68, 2])
 
             # Convert the locations from local face area to the global image.
-            marks *= refined[:, 2, None]
-            marks = marks.reshape([num_faces, 68, 2])
-            marks[:, :, 0] += refined[:, 0, None]
-            marks[:, :, 1] += refined[:, 1, None]
+            marks *= (face[2] - face[0])
+            marks[:, 0] += face[0]
+            marks[:, 1] += face[1]
 
             # Step 3: Try pose estimation with 68 points.
-            poses = [pose_estimator.solve_pose_by_68_points(m) for m in marks]
+            pose = pose_estimator.solve_pose_by_68_points(marks)
 
             tm.stop()
 
@@ -102,19 +98,22 @@ def run():
             # pose on the frame in realtime.
 
             # Do you want to see the pose annotation?
-            for pose in poses:
-                pose_estimator.draw_annotation_box(
-                    frame, pose[0], pose[1], color=(0, 255, 0))
+            pose_estimator.draw_annotation_box(
+                frame, pose[0], pose[1], color=(0, 255, 0))
 
-                # Do you want to see the axes?
-                pose_estimator.draw_axes(frame, pose[0], pose[1])
+            # Do you want to see the axes?
+            pose_estimator.draw_axes(frame, pose[0], pose[1])
 
             # Do you want to see the marks?
-            for m in marks:
-                mark_detector.visualize(frame, m, color=(0, 255, 0))
+            mark_detector.visualize(frame, marks, color=(0, 255, 0))
 
             # Do you want to see the face bounding boxes?
-            face_detector.visualize(frame, refined)
+            face_detector.visualize(frame, faces)
+
+        # Draw the FPS on screen.
+        cv2.rectangle(frame, (0, 0), (90, 30), (0, 0, 0), cv2.FILLED)
+        cv2.putText(frame, f"FPS: {tm.getFPS():.0f}", (10, 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
 
         # Show preview.
         cv2.imshow("Preview", frame)
--- utils.py
+++ utils.py
@@ -3,20 +3,34 @@ import numpy as np
 
 
 def refine(boxes, max_width, max_height, shift=0.1):
+    """Refine the face boxes to suit the face landmark detection's needs.
+
+    Args:
+        boxes: [[x1, y1, x2, y2], ...]
+        max_width: Value larger than this will be clipped.
+        max_height: Value larger than this will be clipped.
+        shift (float, optional): How much to shift the face box down. Defaults to 0.1.
+
+    Returns:
+       Refined results.
+    """
     refined = boxes.copy()
+    width = refined[:, 2] - refined[:, 0]
+    height = refined[:, 3] - refined[:, 1]
 
     # Move the boxes in Y direction
-    shift = refined[:, 3] * shift
+    shift = height * shift
+    refined[:, 1] += shift
     refined[:, 3] += shift
-    center_x = refined[:, 0] + refined[:, 2] / 2
-    center_y = refined[:, 1] + refined[:, 3] / 2
+    center_x = (refined[:, 0] + refined[:, 2]) / 2
+    center_y = (refined[:, 1] + refined[:, 3]) / 2
 
     # Make the boxes squares
-    _square_sizes = np.maximum(refined[:, 2], refined[:, 3])
-    refined[:, 0] = center_x - _square_sizes / 2
-    refined[:, 1] = center_y - _square_sizes / 2
-    refined[:, 2] = _square_sizes
-    refined[:, 3] = _square_sizes
+    square_sizes = np.maximum(width, height)
+    refined[:, 0] = center_x - square_sizes / 2
+    refined[:, 1] = center_y - square_sizes / 2
+    refined[:, 2] = center_x + square_sizes / 2
+    refined[:, 3] = center_y + square_sizes / 2
 
     # Clip the boxes for safety
     refined[:, 0] = np.clip(refined[:, 0], 0, max_width)
